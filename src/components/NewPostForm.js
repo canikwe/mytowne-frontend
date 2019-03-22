ab0d41d9219234b61181c38fbcc02cb0159c0ea1
canikwe <c.anikwe@gmail.com>
2019-03-22 16:40:34 -0400

    track new post form

---
+++ src/components/NewPostForm.js
@@ -0,0 +1,203 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+const currencies = [
+  {
+    value: 'USD',
+    label: '$',
+  },
+  {
+    value: 'EUR',
+    label: '€',
+  },
+  {
+    value: 'BTC',
+    label: '฿',
+  },
+  {
+    value: 'JPY',
+    label: '¥',
+  },
+];
+
+class TextFields extends React.Component {
+  state = {
+    title: '',
+    age: '',
+    content: 'Controlled',
+    currency: 'EUR',
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="standard-name"
+          label="Title"
+          className={classes.textField}
+          value={this.state.title}
+          onChange={this.handleChange('title')}
+          margin="normal"
+        />
+
+
+        <TextField
+          id="standard-multiline-flexible"
+          label="Content"
+          multiline
+          rowsMax="20"
+          value={this.state.content}
+          onChange={this.handleChange('content')}
+          className={classes.textField}
+          margin="normal"
+        />
+
+        {/* <TextField
+          id="standard-multiline-static"
+          label="Multiline"
+          multiline
+          rows="4"
+          defaultValue="Default Value"
+          className={classes.textField}
+          margin="normal"
+        /> */}
+
+
+        <TextField
+          id="standard-with-placeholder"
+          label="With placeholder"
+          placeholder="Placeholder"
+          className={classes.textField}
+          margin="normal"
+        />
+
+        <TextField
+          id="standard-textarea"
+          label="With placeholder multiline"
+          placeholder="Placeholder"
+          multiline
+          className={classes.textField}
+          margin="normal"
+        />
+
+        <TextField
+          id="standard-number"
+          label="Number"
+          value={this.state.age}
+          onChange={this.handleChange('age')}
+          type="number"
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          margin="normal"
+        />
+
+        <TextField
+          id="standard-search"
+          label="Search field"
+          type="search"
+          className={classes.textField}
+          margin="normal"
+        />
+
+        <TextField
+          id="standard-select-currency"
+          select
+          label="Select"
+          className={classes.textField}
+          value={this.state.currency}
+          onChange={this.handleChange('currency')}
+          SelectProps={{
+            MenuProps: {
+              className: classes.menu,
+            },
+          }}
+          helperText="Please select your currency"
+          margin="normal"
+        >
+          {currencies.map(option => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          id="standard-select-currency-native"
+          select
+          label="Native select"
+          className={classes.textField}
+          value={this.state.currency}
+          onChange={this.handleChange('currency')}
+          SelectProps={{
+            native: true,
+            MenuProps: {
+              className: classes.menu,
+            },
+          }}
+          helperText="Please select your currency"
+          margin="normal"
+        >
+          {currencies.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </TextField>
+        <TextField
+          id="standard-full-width"
+          label="Label"
+          style={{ margin: 8 }}
+          placeholder="Placeholder"
+          helperText="Full width!"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+
+        <TextField
+          id="standard-bare"
+          className={classes.textField}
+          defaultValue="Bare"
+          margin="normal"
+        />
+      </form>
+    );
+  }
+}
+
+TextFields.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TextFields);
