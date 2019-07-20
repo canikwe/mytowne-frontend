048a7c63db2c77ccefeda0845394c300c7984635
canikwe <c.anikwe@gmail.com>
2019-07-20 18:48:36 -0400

    refactor TagContainer from class to functional component

--- src/components/ReactTagAutoComplete.js
+++ src/components/ReactTagAutoComplete.js
@@ -1,40 +1,15 @@
 import React, { PureComponent } from 'react'
 import ReactTags from 'react-tag-autocomplete'
-import Tags from '../components/displayTags'
 
-
-class TagContainer extends PureComponent {
-  constructor(props){
-    super(props)
-    this.state = {
-      tags: [
-        { id: 1, name: "Apples" },
-        { id: 2, name: "Pears" }
-      ],
-      // suggestions: [
-      //   { id: 3, name: "Bananas" },
-      //   { id: 4, name: "Mangos" },
-      //   { id: 5, name: "Lemons" },
-      //   { id: 6, name: "Apricots" }
-      // ]
-    }
-  }
-
-
-
-
-  render() {
-    console.log(this.props)
-    const { tags, tagSuggestions, handleTagDelete, handleTagAddition } = this.props
-    return(
-      <ReactTags
-        allowNew
-        tags={tags}
-        suggestions={tagSuggestions}
-        handleDelete={handleTagDelete}
-        handleAddition={handleTagAddition} />
-    )
-  }
+const TagContainer = ({ tags, tagSuggestions, handleTagDelete, handleTagAddition }) => {
+  return(
+    <ReactTags
+      allowNew
+      tags={tags}
+      suggestions={tagSuggestions}
+      handleDelete={handleTagDelete}
+      handleAddition={handleTagAddition} />
+  )
 }
 
 export default TagContainer
