257f3736be791fb116e549e11d72d68e56836d98
canikwe <c.anikwe@gmail.com>
2019-08-13 20:47:11 -0400

    remove Material from Profile

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,33 +1,6 @@
 import React, { PureComponent } from 'react';
 import CardContainer from '../components/CardContainer'
 import { Link } from 'react-router-dom'
-//Button style imports
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles'
-import BackIcon from '@material-ui/icons/ArrowBack'
-import EditIcon from '@material-ui/icons/Edit'
-import IconButton from '@material-ui/core/IconButton'
-
-//Modal style imports
-import Dialog from '@material-ui/core/Dialog'
-import DialogActions from '@material-ui/core/DialogActions'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogContentText from '@material-ui/core/DialogContentText'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import Slide from '@material-ui/core/Slide'
-import Grid from '@material-ui/core/Grid'
-
-const styles = theme => ({
-  media: {
-    height: 'auto',
-    maxWidth: '75%',
-    margin: theme.spacing.unit,
-  },
-})
-
-const transition = (props) => {
-  return <Slide direction="up" {...props} />;
-}
 
 class Profile extends PureComponent {
   state = { open: true }
@@ -41,57 +14,35 @@ class Profile extends PureComponent {
   }
 
   render() {
-    const { classes, user, userPosts } = this.props
+    const { user, userPosts } = this.props
 
     return (
-      <Dialog
-        open={this.state.open}
-        TransitionComponent={transition}
-        keepMounted
-        fullWidth
-        maxWidth={false}
-        // onClose={this.handleClose}
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <Grid container style={{height: 'auto'}}>
-          <Grid item xs={5} style={{textAlign: 'center'}}>
-            {user.avatar !== "" ?
-              <img className={classes.media} src={user.avatar} alt="user avatar"/> : null}
-          </Grid>
-          <Grid item xs={7}>
-            <DialogTitle id="alert-dialog-slide-title">
-              {user.name}&nbsp;
-            </DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-slide-description">
-                {user.bio}
-              </DialogContentText>
-            </DialogContent>
-          </Grid>
+      <React.Fragment>
+        {
+          user.avatar !== "" ?
+            <img src={user.avatar} alt="user avatar"/> 
+          : null
+        }
+        <h2>{user.name}&nbsp;</h2>
+        <p>{user.bio}</p>
 
-          <Grid item xs={12}>
-            <div style={{width: '100%', height: '5px'}}/>
-            {user.posts ? (
+        <div>
+          {
+            user.posts ? (
               <CardContainer posts={ userPosts } name={ user.name } avatar={ user.avatar } />
-            ) : null}
-            <DialogActions>
-              <IconButton component={ Link } to={'/'} onClick={this.handleClose} color="primary" className={classes.button} aria-label="Back">
-                <BackIcon />
-              </IconButton>
-              <IconButton component={ Link } to={`/profile/edit`} onClick={this.handleClose} color="primary" className={classes.button} aria-label="Edit">
-                <EditIcon />
-              </IconButton>
-            </DialogActions>
-          </Grid>
-        </Grid>
-      </Dialog>
+            ) 
+            : null
+          }
+          <Link to={'/'} onClick={this.handleClose} color="primary" >
+            Back
+          </Link>
+          <Link to={`/profile/edit`} onClick={this.handleClose} >
+            Edit
+          </Link>
+        </div>
+      </React.Fragment >
     )
   }
 }
 
-Profile.propTypes = {
-  classes: PropTypes.object.isRequired,
-}
-
-export default withStyles(styles)(Profile)
+export default Profile
