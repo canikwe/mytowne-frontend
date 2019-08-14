74b8f923de2c04a55683681d2ae087810e451a97
canikwe <c.anikwe@gmail.com>
2019-08-13 20:12:50 -0400

    remove Material from login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,49 +1,6 @@
 import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
-import Avatar from '@material-ui/core/Avatar'
-import Button from '@material-ui/core/Button'
-import FormControl from '@material-ui/core/FormControl'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Checkbox from '@material-ui/core/Checkbox'
-import Input from '@material-ui/core/Input'
-import InputLabel from '@material-ui/core/InputLabel'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
-import withStyles from '@material-ui/core/styles/withStyles'
+import '../styles/Login.css'
 
-const styles = theme => ({
-  main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 20,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing.unit,
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
-})
 
 class Login extends PureComponent {
   state = {
@@ -58,80 +15,49 @@ class Login extends PureComponent {
   }
 
   render() {
-    const { classes, handleLogin } = this.props
+    const { handleLogin } = this.props
 
     return (
-      <main className={classes.main}>
-        <Paper className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Log In
-          </Typography>
-          <form className={classes.form}>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="username">Username</InputLabel>
-              <Input
+      <main>
+        <div className='login-div'>
+          <h3>Login</h3><br />
+          <form className='login-form'>
+              <label htmlFor="username">Username</label>
+              <input
                 id="username"
                 name="username"
                 value={this.state.username}
                 onChange={this.handleChange}
-                autoComplete="username"
-                autoFocus
-              />
-            </FormControl>
+              /><br />
 
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input
+              <label htmlFor="password">Password</label>
+              <input
                 id="password"
                 name="password"
                 type="password"
                 value={this.state.password}
                 onChange={this.handleChange}
-                autoComplete="current-password"
-              />
-            </FormControl>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember Me"
-            />
-            <Button
+              /><br />
+            <button
               type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
               onClick={(e) => {
                 e.preventDefault()
                 handleLogin({user: {
                   username: this.state.username, password: this.state.password
                 }
-                })}
+                }
+                )}
               }
             >
-              Log In
-            </Button>
+              Log In:
+            </button>
           </form>
-        </Paper>
-        <br/>
-
-        {/* <Paper className={classes.paper}>
-          <Typography variant="h5">
-            About MyTowne:
-          </Typography>
-          <Typography variant="body1">
-            Information and description all goes in here. Will come back to format this to be side-by-side with the login form.
-          </Typography>
-        </Paper> */}
+        </div>
       </main>
-    );
+    )
   }
 }
 
-Login.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
-export default withStyles(styles)(Login);
+
+export default Login
