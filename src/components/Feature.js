687cd4a494e2611de53a2d02d155d5ef799ca2f9
canikwe <c.anikwe@gmail.com>
2019-08-20 16:47:53 -0400

    add Feature and Footer components for tracking

---
+++ src/components/Feature.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import '../images'
+
+const Feature = () => {
+  return(
+    <React.Fragment>
+      <img
+      id='feature'
+      src='/images/apartment-architecture-city-415687.jpg' alt='feature-img' />
+    </React.Fragment>
+  )
+}
+
+export default Feature
---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+const Footer = () => {
+  return(
+    <div id='footer'>
+      <div id='footer-content'>
+        <h3>
+          About the Site
+        </h3>
+        <p>Con panna kopi-luwak chicory frappuccino at rich caramelization. Beans dark cream aged trifecta est as beans siphon wings latte. Variety, coffee latte robust iced coffee café au lait. And whipped and grounds, macchiato, fair trade, shop mazagran grounds organic percolator. Arabica medium spoon saucer fair trade extraction americano irish.</p>
+      </div>
+
+      <div id='footer-links'>
+        <h3>
+          Links
+        </h3>
+        <ul>
+          <li>
+            Some Link
+          </li>
+          <li>
+            Some other link
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
