4b7aa0dda8c81305ccd5314edcdb73b00bbabc41
canikwe <c.anikwe@gmail.com>
2019-08-20 07:13:24 -0400

    add dropdown to navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,18 +4,24 @@ import '../styles/NavBar.css'
 
 const NavigationBar = ({handleLogout, loggedIn, searchInput, handleSearch}) => {
   return(
-    <ul id='nav'>
-      <li><Link to='/'>Home</Link></li>
-      {loggedIn ? 
-      <React.Fragment>
-        <li><Link to='/profile'>My Profile</Link></li>
-        <li><Link to='/profile/edit'>My Account</Link></li>
-          <li><Link to='/posts/new'>Create a New Post</Link></li>
-        <li>Search: <input type='text' value={searchInput} onChange={handleSearch} /></li>
-      </React.Fragment>
-      : null}
-      <li id='logout'><Link to='/login' onClick={handleLogout}>Logout</Link></li>
-    </ul>
+    <div id='nav-container'>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        {loggedIn ? 
+        <React.Fragment>
+          <li id='search'>Search: <input className='search-bar' type='text' value={searchInput} onChange={handleSearch} /></li>
+          <li id='menu'>Menu
+            <ul class='menu-item'>
+              <li><Link to='/profile'>My Profile</Link></li>
+              <li><Link to='/profile/edit'>My Account</Link></li>
+              <li><Link to='/posts/new'>Create a New Post</Link></li>
+              <li><Link to='/login' onClick={handleLogout}>Logout</Link></li>
+            </ul>
+          </li>
+        </React.Fragment>
+        : null}
+      </ul>
+    </div>
   )
 }
 
