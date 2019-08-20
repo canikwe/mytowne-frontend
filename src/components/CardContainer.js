c78d1cb2172610b82363f4055e7e5d4fac342d5e
canikwe <c.anikwe@gmail.com>
2019-08-19 21:52:02 -0400

    remove Material from Card Container

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Cards from '../containers/Cards'
-import Grid from '@material-ui/core/Grid'
 import '../styles/Cards.css'
 
 
@@ -8,9 +7,9 @@ import '../styles/Cards.css'
 const CardContainer = ({ classes, posts, name, avatar }) => {
 
   return (
-    <Grid container direction="row" justify="space-evenly" alignItems="center">
+    <div className='card-container'>
       {posts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)).map((post) => name ? <Cards post={post} name={name} avatar={avatar} key={post.id} /> : <Cards post={post} key={post.id} /> )}
-    </Grid>
+    </div>
   )
 }
 
