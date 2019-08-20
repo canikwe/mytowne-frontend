c21c38ed63796b6a7cd1160dd33012197a8e2df9
canikwe <c.anikwe@gmail.com>
2019-08-20 03:04:35 -0400

    add card header and likes container

--- src/containers/Cards.js
+++ src/containers/Cards.js
@@ -25,23 +25,30 @@ class Cards extends PureComponent {
 
   render() {
     const { post } = this.props
+    console.log(post)
+    const postDate = moment(post.created_at)
     
     return (
       <Fragment>
-        <div 
-          className='card'
-          onClick={() => console.log('Someday I shall do something!')}
-        >
+        <div className='card'>
+          <div className='card-header'>
+            <img src={!!post.user.avatar ? post.user.avatar : 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png'} alt='avatar' className='card-avatar'/>
+            <p className='card-date'>{ !postDate.isSame(moment(), 'week') ? postDate.calendar(null, {sameElse: 'DD-MMM'}) : postDate.fromNow() }</p>
+          </div>
           <div className='img-container'>
-            <img src={!!post.img ? post.img : 'https://imgplaceholder.com/350x225/ff7f7f/333333/fa-image'} className ='card-image' alt={post.title} />
-            <i className='material-icons favorite' onClick={this.likePost}>{this.state.favorite}</i>
+            <Link className='card-link' to={`/posts/${post.id}`}>
+              <img src={!!post.img ? post.img : 'https://imgplaceholder.com/350x225/ff7f7f/333333/fa-image'} className ='card-image' alt={post.title} />
+            </Link>
           </div>
-          <div className='card-content'>
-            <h4>{post.title}</h4>
-            <p>{ moment(post.created_at).calendar() }</p>
-            {/* <p>{post.content}</p> */}
-            <Link to={`/posts/${post.id}`}>Click for More...</Link>
-          </div>
+            <div className='card-content'>
+              <h4 className='card-title'>{post.title}</h4>
+              <div className='likes-container'>
+                <i className='material-icons favorite' onClick={this.likePost}>{this.state.favorite}</i>
+                <p>Likes</p>
+              </div>
+              {/* <p>{post.content}</p> */}
+              {/* <Link to={`/posts/${post.id}`}>Click for More...</Link> */}
+            </div>
           <hr/>
           <div className='tag-container'>
             {post.post_tags.map((tag) => {
