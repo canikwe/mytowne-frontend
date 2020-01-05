39b774e367e3949fc572c816486ab6b0753fae3b
canikwe <c.anikwe@gmail.com>
2020-01-04 22:09:32 -0500

    update homepage grid

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Feature from '../components/Feature'
 import TopPosts from '../components/TopPosts'
 import HomeCard from '../components/HomeCard'
-import { Button } from 'antd'
+import { Button, Row, Col } from 'antd'
 
 class Home extends Component {
     constructor(){
@@ -28,12 +28,28 @@ class Home extends Component {
         const { user, posts, followedPosts } = this.props
         console.log(followedPosts)
         return(
-            <div id='home-layout'>
-                <Feature user={ user } />
-                {this.showFollowedPosts().map(p => <HomeCard key={p.id} post={p}/>)}
-                <Button onClick={ this.showMorePosts } type="primary">More Posts</Button>
-                <TopPosts posts={posts}/>
-            </div>
+            <>
+            <Row gutter={[8, 16]}>
+                    <Col span={24}>
+                        <Feature user={ user } />
+                    </Col>
+                </Row>
+            {/* <div id='home-layout'> */}
+            <Row gutter={[8, 16]} type="flex" justify="space-around" align="top">
+                <Col span={12}>
+                    {this.showFollowedPosts().map(p => <HomeCard key={p.id} post={p}/>)}
+                </Col>
+                <Col span={8}>
+                    <TopPosts posts={posts}/>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={12} offset={1}>
+                    <Button onClick={ this.showMorePosts } type="primary">More Posts</Button>
+                </Col>
+            </Row>
+            {/* </div> */}
+            </>
         )
     }
 }
